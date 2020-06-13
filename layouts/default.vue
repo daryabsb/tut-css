646d52091e45d80ff641168ca364bf1cbd57623a
daryabsb <daryabsb@gmail.com>
2020-06-13 10:28:40 +0300

    Converted the classes to BEM

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+  </div>
+</template>
+<script>
+import Header from "~/components/Header";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: "Lato", sans-serif !important;
+  font-weight: 400;
+  /* font-size: 16px; */
+  line-height: 1.7;
+  color: #777;
+  padding: 3rem;
+
+  box-sizing: border-box;
+}
+</style>
