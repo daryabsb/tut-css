ba2b3923c5e6ff0344ecc5bdd0c119985b6e64d4
daryabsb <daryabsb@gmail.com>
2020-06-17 11:47:24 +0300

    Added the about styles - failed on composition

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,47 @@
 <template>
   <main>
     <section class="section-about">
-      <div class="u-center-text">
+      <div class="u-center-text u-margin-bottom-large">
         <h2 class="heading-secondary">Exciting tour for adventurer people</h2>
+      </div>
+      <div class="row">
+        <div class="col-1-of-2">
+          <h3
+            class="heading-tertiary u-margin-bottom-small"
+          >You're going to fall in love with nature!</h3>
+          <p
+            class="paragraph"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo commodi vel cum laudantium voluptatem necessitatibus provident itaque voluptate. Laudantium neque ducimus id asperiores quo qui numquam reiciendis. Placeat, aspernatur totam.</p>
+          <h3
+            class="heading-tertiary u-margin-bottom-small"
+          >Live adventure like you never have before!</h3>
+          <p
+            class="paragraph"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo commodi vel cum laudantium voluptatem necessitatibus provident itaque voluptate. Laudantium neque ducimus id asperiores quo qui numquam reiciendis. Placeat, aspernatur totam.</p>
+          <a href="#" class="btn-text">Learn More &rarr;</a>
+        </div>
+        <div class="col-1-of-2">
+          <div class="composition">
+            <h1>Darya</h1>
+          </div>
+          <div class="composition">
+            <img
+              src="/img/nat-1.jpg"
+              alt="photo 1"
+              class="composition__photo composition__photo--p1"
+            />
+            <img
+              src="/img/nat-2.jpg"
+              alt="photo 2"
+              class="composition__photo composition__photo--p2"
+            />
+            <img
+              src="/img/nat-3.jpg"
+              alt="photo 3"
+              class="composition__photo composition__photo--p3"
+            />
+          </div>
+        </div>
       </div>
     </section>
   </main>
