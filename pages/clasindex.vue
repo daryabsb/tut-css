4f615318d7b5238084acac4b8b8039a2952dc161
daryabsb <daryabsb@gmail.com>
2020-06-22 16:26:00 +0300

    Tried some new css bars

--- pages/clasindex.vue
+++ pages/clasindex.vue
@@ -1,5 +1,311 @@
 <template>
-  <h1>DARYA</h1>
+  <div class="wrapper">
+    <header class="side-header">
+      <div class="topbar">
+        <h4>en</h4>
+      </div>
+      <div class="middlebar">logo</div>
+      <div class="relative"></div>
+    </header>
+
+    <div class="sidebar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Pages</a>
+        </li>
+        <li>
+          <a href="#">Social</a>
+        </li>
+      </ul>
+    </div>
+    <div class="main">
+      <div class="menu">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Pages</a>
+          </li>
+          <li>
+            <a href="#">Social</a>
+          </li>
+        </ul>
+      </div>
+      <div class="text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aspernatur autem, ipsum at eos laboriosam quod exercitationem illo enim odit, tempore maiores mollitia sunt incidunt a laborum inventore in. Saepe.
+          <br />
+          <br />
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +314,102 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.wrapper {
+  box-sizing: border-box;
+  // position: relative;
+  margin: 0;
+  padding: 0;
+}
+.side-header {
+}
+.relative {
+  position: relative;
+  padding: 0;
+  top: 15rem;
+}
+.topbar {
+  // display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 3rem;
+  width: 100%;
+  background-color: purple;
+}
+.middlebar {
+  // display: block;
+  position: absolute;
+  top: 3rem;
+  left: 0;
+  height: 12rem;
+  width: 100%;
+  background-color: rosybrown;
+}
+.menu {
+  // display: block;
+  position: sticky;
+  top: 0;
+  height: 4rem;
+  width: 100%;
+  background-color: #eeeeee;
+  ul {
+    display: flex;
+    padding: 1rem;
+    list-style: none;
+
+    li {
+      color: #000;
+      text-decoration: none;
+      font-size: 1.3rem;
+
+      &:not(:last-child) {
+        padding-right: 2rem;
+      }
+    }
+  }
+}
+.sidebar {
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 15rem;
+  width: 15rem;
+  height: 100%;
+  background-color: plum;
+  padding: 2rem 0rem;
+
+  ul {
+    list-style: none;
+
+    li {
+      text-align: center;
+      padding: 2rem;
+      cursor: pointer;
+      &:hover {
+        background-color: purple;
+      }
+      a,
+      a:link,
+      a:visited {
+        color: powderblue;
+
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        font-weight: 700;
+      }
+    }
+  }
+}
+.main {
+  background-color: powderblue;
+
+  margin: 12rem;
+  top: 15rem;
+  left: 15rem;
+}
+.text {
+  padding: 2rem 2rem;
+}
 </style>
