53d976e8cdde10a8db62e4a02f8672369a4aaa66
daryabsb <daryabsb@gmail.com>
2020-06-28 12:54:18 +0300

    Completed Natours ready to next...

--- components/partials/Footer.vue
+++ components/partials/Footer.vue
@@ -1,7 +1,18 @@
 <template>
   <footer class="footer">
     <div class="footer__logo-box">
-      <img src="/img/logo-green-2x.png" alt="full logo" class="footer__logo" />
+      <picture class="footer__logo">
+        <source
+          srcset="/img/logo-green-small-1x.png 1x, /img/logo-green-small-2x.png 2x"
+          media="(max-width: 37.5em)"
+          src="/img/logo-green-small-2x.png"
+        />
+        <img
+          srcset="/img/logo-green-1x.png 1x, /img/logo-green-2x.png 2x"
+          alt="full logo"
+          src="/img/logo-green-2x.png"
+        />
+      </picture>
     </div>
     <div class="row">
       <div class="col-1-of-2">
@@ -28,8 +39,11 @@
       <div class="col-1-of-2">
         <p class="footer__copyright">
           Built my
-          <a href="#" class="footer__link">Darya Ibrahim</a> advanced combination of nuxt.js and my own css/html library without using external css frameworks such as bootstrap!
-          <br />Copyright &copy; by Darya Ibrahim
+          <a href="#" class="footer__link">Darya Ibrahim</a> advanced
+          combination of nuxt.js and my own css/html library without using
+          external css frameworks such as bootstrap!
+          <br />Copyright &copy; by
+          Darya Ibrahim
         </p>
       </div>
     </div>
