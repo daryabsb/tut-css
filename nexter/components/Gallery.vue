37caf2fe087cb4570608fd7a2bdd9a8a4d65f751
daryabsb <daryabsb@gmail.com>
2020-07-06 11:25:12 +0300

    Added the gallery part

--- nexter/components/Gallery.vue
+++ nexter/components/Gallery.vue
@@ -1,3 +1,8 @@
 <template>
-  <section class="gallery">Gallery</section>
+  <section class="gallery">
+    <figure v-for="id in 14" :key="id" :class="`gallery__item gallery__item--${id}`">
+      <img :src="`/img/gal-${id}.jpeg`" :alt="`Gallery image ${id}`" class="gallery__img" />
+    </figure>
+  </section>
 </template>
+
